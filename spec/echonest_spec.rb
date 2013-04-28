22ea367224d905a71c457b4f84fb4a55b53b995a
Tim Case <tim@endpoint.com>
2013-04-27 17:03:17 -0700

    add cache specs

--- spec/echonest_spec.rb
+++ spec/echonest_spec.rb
@@ -7,22 +7,21 @@ describe Echonest do
       expect(Echonest.client).to be_a Echonest::Client
     end
 
-    # context "when the options don't change" do
-    #   it "caches the client" do
-    #     expect(Twitter.client).to eq Twitter.client
-    #   end
-    # end
+    context "when the options don't change" do
+      it "caches the client" do
+        expect(Echonest.client).to eq Echonest.client
+      end
+    end
 
-    # context "when the options change" do
-    #   it "busts the cache" do
-    #     client1 = Twitter.client
-    #     Twitter.configure do |config|
-    #       config.consumer_key = 'abc'
-    #       config.consumer_secret = '123'
-    #     end
-    #     client2 = Twitter.client
-    #     expect(client1).not_to eq client2
-    #   end
-    # end
+    context "when the options change" do
+      it "busts the cache" do
+        client1 = Echonest.client
+        Echonest.configure do |config|
+          config.api_key = 'abc'
+        end
+        client2 = Echonest.client
+        expect(client1).not_to eq client2
+      end
+    end
   end
 end
