e9cdaae0c36d61bfcee670b3fbd71dc94876d281
Tim Case <tim@2drops.net>
2013-05-18 18:58:44 -0700

    Add taste profile update

--- lib/echonest/api/taste_profile.rb
+++ lib/echonest/api/taste_profile.rb
@@ -22,6 +22,24 @@ module Echonest
         response = send(:post, '/api/v4/catalog/create', options)
         Echonest::TasteProfile.fetch_or_new(response[:body][:response])
       end
+
+      # Updates (adds or deletes) items from a taste profile. The body of the post should include an item block that describes modifications to the taste profile.
+      #
+      # @see http://developer.echonest.com/docs/v4/taste_profile.html#update
+      # @authentication Requires api key
+      # @raise [Echonest::Error::Unauthorized] Error raised when supplied api key is not valid.
+      # @raise [Echonest::Error::Unauthorized] Error raised when supplied user credentials are not valid.
+      # @return [Echonest::TasteProfile] The taste profile.
+      # @param options [Hash] A customizable set of options.
+      # @option options [String] :id The ID of the taste profile. Required. Example: 'CANVFPJ131839D8144'
+      # @option options [String] :data The data to be uploaded.  Required, Must be JSON format.  See Echonest API docs for data options
+      #
+      # @example taste_profile_update
+      #   Echonest.taste_profile_update(:name => "Favorite artists of Paul", :type => 'artist')
+      def taste_profile_update(options={})
+        response = send(:post, '/api/v4/catalog/update', options.merge({:data_type => 'json'}))
+        Echonest::TasteProfile.fetch_or_new(response[:body][:response])
+      end
     end
   end
 end
--- lib/echonest/taste_profile.rb
+++ lib/echonest/taste_profile.rb
@@ -2,6 +2,10 @@ require 'echonest/identity'
 
 module Echonest
     class TasteProfile < Echonest::Identity
-      attr_reader :name, :type
+      attr_reader :name, :ticket, :type
+      
+      def id
+        @attrs[:id] || self.ticket
+      end
     end
 end
