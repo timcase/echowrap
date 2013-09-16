474f71b465d8d545c141700bf915b2cb147bb6c1
Tim Case <tim@2drops.net>
2013-09-16 09:26:07 -0700

    update gem dependencies

--- lib/echowrap/version.rb
+++ lib/echowrap/version.rb
@@ -1,8 +1,8 @@
 module Echowrap
   class Version
     MAJOR = 0 unless defined? Echowrap::Version::MAJOR
-    MINOR = 0 unless defined? Echowrap::Version::MINOR
-    PATCH = 3 unless defined? Echowrap::Version::PATCH
+    MINOR = 1 unless defined? Echowrap::Version::MINOR
+    PATCH = 0 unless defined? Echowrap::Version::PATCH
     PRE = nil unless defined? Echowrap::Version::PRE
 
     class << self
