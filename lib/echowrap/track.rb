9f8e1c92927e608eb372287eadc0e0ef8b78ef32
aandis <abhishekrm@iitrpr.ac.in>
2015-07-24 20:57:24 +0530

    Added album attributes to track. Fixes #13

--- lib/echowrap/track.rb
+++ lib/echowrap/track.rb
@@ -2,13 +2,13 @@ require 'echowrap/base'
 
 module Echowrap
     class Track < Echowrap::Base
-      attr_reader :analysis_channels, :analysis_sample_rate, :analyzer_version, :artist,
-                  :attributes, :audio_md5, :bitrate, :catalog, :code_version, :codestring,
-                  :decoder, :duration, :echoprint_version, :echoprintstring, :end_of_fade_in,
-                  :foreign_id, :foreign_release_id, :id, :key, :key_confidence, :loudness,
-                  :md5, :mode, :mode_confidence, :num_samples, :offset_seconds, :preview_url,
-                  :release, :release_image, :sample_md5, :samplerate, :song_id,
-                  :start_of_fade_out, :status, :synch_version, :synchstring, :tempo,
+      attr_reader :album_date, :album_name, :album_type, :analysis_channels, :analysis_sample_rate,
+                  :analyzer_version, :artist, :attributes, :audio_md5, :bitrate, :catalog,
+                  :code_version, :codestring, :decoder, :duration, :echoprint_version,
+                  :echoprintstring, :end_of_fade_in, :foreign_id, :foreign_release_id, :id,
+                  :key, :key_confidence, :loudness, :md5, :mode, :mode_confidence, :num_samples,
+                  :offset_seconds, :preview_url, :release, :release_image, :sample_md5, :samplerate,
+                  :song_id, :start_of_fade_out, :status, :synch_version, :synchstring, :tempo,
                   :tempo_confidence, :time_signature, :time_signature_confidence, :title,
                   :window_seconds
 
